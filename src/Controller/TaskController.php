09d93a16327d005a4fa60981977e90b65a7aa976
Teko13 <ffabrice999@gmail.com>
2024-03-06 17:34:14 +0100

    upgrade project to symfony 6

---
+++ src/Controller/TaskController.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Task;
+use App\Form\TaskType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class TaskController extends AbstractController
+{
+    public function __construct(private EntityManagerInterface $em) {}
+    #[Route('/tasks', name: 'tasks_list')]
+    public function tasksList(): Response
+    {
+        return $this->render('task/list.html.twig', [
+            'tasks' => $this->em->getRepository(Task::class)->findAll(),
+        ]);
+    }
+    #[Route("/tasks/create", name: "create_task")]
+    public function createAction(Request $request): Response
+    {
+        $task = new Task();
+        $form = $this->createForm(TaskType::class, $task);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $this->em->persist($task);
+            $this->em->flush();
+
+            $this->addFlash('success', 'La tâche a été bien été ajoutée.');
+
+            return $this->redirectToRoute('tasks_list');
+        }
+
+        return $this->render('task/create.html.twig', ['form' => $form->createView()]);
+    }
+    #[Route("/tasks/{id}/toggle", name: "toggle_task")]
+    public function toggleTaskAction(Request $request):Response
+    {
+        $task = $this->em->getRepository(Task::class)->findOneBy(["id" => $request->attributes->get("id")]);
+        $task->toggle(!$task->isDone());
+        $this->em->persist($task);
+        $this->em->flush();
+
+        $this->addFlash('success', sprintf('La tâche %s a bien été marquée comme faite.', $task->getTitle()));
+
+        return $this->redirectToRoute('tasks_list');
+    }
+    #[Route("/tasks/{id}/delete", name: "delete_task")]
+    public function deleteTaskAction(Request $request):Response
+    {
+        $task = $this->em->getRepository(Task::class)->findOneBy(["id"=>$request->attributes->get("id")]);
+        $this->em->remove($task);
+        $this->em->flush();
+
+        $this->addFlash('success', 'La tâche a bien été supprimée.');
+
+        return $this->redirectToRoute('tasks_list');
+    }
+    #[Route("/tasks/{id}/edit", name: "edit_task")]
+    public function editAction(Request $request):Response
+    {
+        $task = $this->em->getRepository(Task::class)->findOneBy(["id"=>$request->attributes->get("id")]);
+        $form = $this->createForm(TaskType::class, $task);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->em->persist($task);
+            $this->em->flush();
+
+            $this->addFlash('success', 'La tâche a bien été modifiée.');
+
+            return $this->redirectToRoute('tasks_list');
+        }
+
+        return $this->render('task/edit.html.twig', [
+            'form' => $form->createView(),
+            'task' => $task,
+        ]);
+    }
+}
---
+++ src/Form/TaskType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Task;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+class TaskType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title', TextType::class, [
+                "constraints" => [new NotBlank(message: "Vous devez saisir un titre")]
+            ])
+            ->add('content', TextType::class, [
+                "constraints" => [new NotBlank(message: "Vous devez saisir du contenu.")]
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Task::class,
+        ]);
+    }
+}
