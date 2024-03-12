c59d0768d94ebdc6dd9f9f857783a26f82ecc7e4
Teko13 <ffabrice999@gmail.com>
2024-03-12 09:11:42 +0100

    unabling task's author modification and task editing tests

--- tests/TaskActionTest.php
+++ tests/TaskActionTest.php
@@ -24,11 +24,32 @@ class TaskActionTest extends TestCase
         $emMock->expects($this->once())->method("flush");
         // Test
         $taskAction = new TaskAction($emMock, $securityMock);
-        $result = $taskAction->saveNewTask(new Task);
+        $result = $taskAction->save(new Task);
         // Assertion
         $this->assertNotNull($result->getAuthor());
         $this->assertEquals($result->getAuthor()->getUsername(), $user->getUsername());
 
 
+    }
+    public function testSaveExistingTask(): void
+    {
+        // Mocking methode dependences
+        $securityMock = $this->getMockBuilder(Security::class)->disableOriginalConstructor()->getMock();
+        $emMock = $this->createMock(EntityManagerInterface::class);
+        $user = new User();
+        $user->setUsername("current_user");
+        $author = new User;
+        $author->setUsername("author");
+        $task = new Task;
+        $task->setAuthor($author);
+        //Mocks configuration
+        $securityMock->method("getUser")->willReturn($user);
+        $emMock->expects($this->once())->method("persist")->with($this->isInstanceOf(Task::class));
+        $emMock->expects($this->once())->method("flush");
+        // Test
+        $taskAction = new TaskAction($emMock, $securityMock);
+        $result = $taskAction->save($task);
+        // Assertion
+        $this->assertEquals($result->getAuthor()->getUsername(), $author->getUsername());
     }
 }
--- tests/TaskControllerTest.php
+++ tests/TaskControllerTest.php
@@ -2,6 +2,7 @@
 
 namespace App\Tests;
 
+use App\Repository\TaskRepository;
 use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use Symfony\Component\HttpFoundation\Response;
@@ -17,7 +18,7 @@ class TaskControllerTest extends WebTestCase
         $router = static::getContainer()->get(RouterInterface::class);
         $redirectionUrl = $router->generate("tasks_list");
         $taskCreationRoute = $router->generate("create_task",[], RouterInterface::ABSOLUTE_PATH);
-        $user = $userRepo->findOneBy(["username" => "coco"]);
+        $user = $userRepo->findOneBy([]);
         $client->loginUser($user);
         $crawler = $client->request('GET', $taskCreationRoute);
         $this->assertResponseIsSuccessful();
@@ -28,6 +29,27 @@ class TaskControllerTest extends WebTestCase
         $this->assertResponseRedirects($redirectionUrl);
         $client->followRedirect();
         $this->assertResponseStatusCodeSame(Response::HTTP_OK);
+        
+    }
+    public function testEdit():void
+    {
+        $client = static::createClient();
+        $taskRepos = static::getContainer()->get(TaskRepository::class);
+        $userRepos = static::getContainer()->get(UserRepository::class);
+        $router = static::getContainer()->get(RouterInterface::class);
+        $task = $taskRepos->findOneBy([]);
+        $taskEditionRoute = $router->generate("edit_task", ["id" => $task->getId()], RouterInterface::ABSOLUTE_PATH);
+        $redirectionUrl = $router->generate("tasks_list");
+        $user = $userRepos->findOneBy([]);
+        $client->loginUser($user);
+        $crawler = $client->request("GET", $taskEditionRoute);
+        $this->assertResponseIsSuccessful();
+        $form = $crawler->selectButton("Modifier")->form();
+        $form["task[title]"] = "title1";
+        $client->submit($form);
+        $this->assertResponseRedirects($redirectionUrl);
+        $client->followRedirect();
+        $this->assertSelectorTextContains("div.alert.alert-success", "Superbe ! La tâche a bien été modifiée.");
         
     }
 }
